f2eaf39fa471b41db38666aad8d1bd926ea6a9b0
skv10 <sameershukla1999@gmail.com>
2025-12-23 18:27:53 +0530

    second commit

--- src/pages/Root.js
+++ src/pages/Root.js
@@ -1,9 +1,29 @@
-import { Outlet, useNavigation } from 'react-router-dom';
+import { Outlet, useLoaderData, useSubmit } from "react-router-dom";
 
-import MainNavigation from '../components/MainNavigation';
+import MainNavigation from "../components/MainNavigation";
+import { useEffect } from "react";
+import { getTokenDuration } from "../util/auth";
 
 function RootLayout() {
   // const navigation = useNavigation();
+  const token = useLoaderData();
+  const submit = useSubmit();
+
+  useEffect(() => {
+    if (!token) {
+      return;
+    }
+
+    if (token === "EXPIRED") {
+      submit(null, { method: "post", action: "/logout" });
+      return;
+    }
+
+    const tokenDuration = getTokenDuration();
+    setTimeout(() => {
+      submit(null, { method: "post", action: "/logout" });
+    }, tokenDuration);
+  }, [token, submit]);
 
   return (
     <>
